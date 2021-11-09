3de105f9f15934479a500cb3f61c1ea798ee2d56
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-11-09 14:52:33 +0100

    Modified Range.java with a Nested class

--- workspace/lab05/src/it/unibo/oop/lab05/ex1/Range.java
+++ workspace/lab05/src/it/unibo/oop/lab05/ex1/Range.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.lab05.ex1;
 
+import java.util.Iterator;
+
 public class Range implements Iterable<String>{
 	
 	private String start;
@@ -11,6 +13,32 @@ public class Range implements Iterable<String>{
 	}
 	
 	public java.util.Iterator<String> iterator() {
-		return new RangeIterator(this.start, this.stop);
-	}	
+		return new RangeIterator2(this.start, this.stop);
+	}
+	
+	private class RangeIterator2 extends Range implements Iterator<String>{
+		
+		private String current;
+		private int count;
+
+		public RangeIterator2(String start, String stop) {
+			super(start, stop);
+			this.current = start;
+			this.count = 0;
+		}
+		
+		public boolean hasNext() {
+			return Integer.parseInt(this.current) < Integer.parseInt(stop);
+		}
+
+		public String next() {
+			if(this.count != 0) {
+				this.current = Integer.toString(Integer.parseInt(this.current) + 1);
+			}
+			this.count++;
+			return this.current;
+		}
+		
+	}
+	
 }
