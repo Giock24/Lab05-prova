db5b49d8cebba51c0f5bcb3409331e2f077ae486
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-11-14 11:20:26 +0100

    Upgraded Range.java with Local (RangeIterator) class

--- workspace/lab05/src/it/unibo/oop/lab05/ex1/Range.java
+++ workspace/lab05/src/it/unibo/oop/lab05/ex1/Range.java
@@ -13,33 +13,35 @@ public class Range implements Iterable<String>{
 	}
 	
 	public java.util.Iterator<String> iterator() {
+		class RangeIterator implements Iterator<String>{
+			
+			private String current;
+			private final String stop;
+			private int count;
+
+			public RangeIterator() {
+				this.current = Range.this.start;
+				this.stop = Range.this.stop;
+				this.count = 0;
+			}
+			
+			public boolean hasNext() {
+				return Integer.parseInt(this.current) < Integer.parseInt(this.stop);
+			}
+
+			public String next() {
+				if(this.count != 0) {
+					this.current = Integer.toString(Integer.parseInt(this.current) + 1);
+				}
+				this.count++;
+				return this.current;
+			}
+			
+		}
+		
 		return new RangeIterator();
 	}
 	
-	private class RangeIterator implements Iterator<String>{
-		
-		private String current;
-		private final String stop;
-		private int count;
-
-		public RangeIterator() {
-			this.current = Range.this.start;
-			this.stop = Range.this.stop;
-			this.count = 0;
-		}
-		
-		public boolean hasNext() {
-			return Integer.parseInt(this.current) < Integer.parseInt(this.stop);
-		}
-
-		public String next() {
-			if(this.count != 0) {
-				this.current = Integer.toString(Integer.parseInt(this.current) + 1);
-			}
-			this.count++;
-			return this.current;
-		}
-		
-	}
+	
 	
 }
